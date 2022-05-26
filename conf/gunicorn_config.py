ccb2bcd57f7feb385b7560ec964f1f962d9961f2
ThinhNguyenHuu <thinhnguyenhuu92@gmail.com>
2022-05-26 11:37:50 +0700

    wrong ip

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,4 +1,4 @@
 command = '/home/ec2-user/workspace/store-5pm-management/env/bin/gunicorn'
 pythonpath = '/home/ec2-user/workspace/store-5pm-management'
-bind = '13.250.113.200:8000'
+bind = '172.31.25.38:8000'
 workers = 2
